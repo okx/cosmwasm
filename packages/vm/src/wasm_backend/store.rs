9e0b8c6455d1c6fc6bcca697fb15111f2dba155f
jianguo <zjg555543@163.com>
2023-05-31 14:16:57 +0800

    update

--- packages/vm/src/wasm_backend/store.rs
+++ packages/vm/src/wasm_backend/store.rs
@@ -2,7 +2,7 @@ use backtrace::Backtrace;
 use std::sync::Arc;
 #[cfg(feature = "cranelift")]
 use wasmer::Cranelift;
-#[cfg(not(feature = "cranelift"))]
+#[cfg(feature = "singlepass")]
 use wasmer::Singlepass;
 use wasmer::{
     wasmparser::Operator, BaseTunables, CompilerConfig, Engine, ModuleMiddleware, Pages, Store,
@@ -10,7 +10,7 @@ use wasmer::{
 };
 use wasmer_middlewares::Metering;
 
-#[cfg((feature = "llvm"))]
+#[cfg(feature = "llvm")]
 use wasmer::LLVM;
 
 use crate::size::Size;
@@ -56,7 +56,7 @@ pub fn make_engine(middlewares: &[Arc<dyn ModuleMiddleware>]) -> Engine {
         compiler.into()
     }
 
-    #[cfg((feature = "llvm"))]
+    #[cfg(feature = "llvm")]
     {
         let mut compiler = LLVM::default();
         println!("-------------LLVM------------------");
@@ -68,17 +68,17 @@ pub fn make_engine(middlewares: &[Arc<dyn ModuleMiddleware>]) -> Engine {
         compiler.into()
     }
 
-    // #[cfg(not(feature = "cranelift"))]
-    // {
-    //     let mut compiler = Singlepass::default();
-    //     println!("-------------Singlepass------------------");
-    //     for middleware in middlewares {
-    //         compiler.push_middleware(middleware.clone());
-    //     }
-    //     compiler.push_middleware(deterministic);
-    //     compiler.push_middleware(metering);
-    //     compiler.into()
-    // }
+    #[cfg(feature = "singlepass")]
+    {
+        let mut compiler = Singlepass::default();
+        println!("-------------Singlepass------------------");
+        for middleware in middlewares {
+            compiler.push_middleware(middleware.clone());
+        }
+        compiler.push_middleware(deterministic);
+        compiler.push_middleware(metering);
+        compiler.into()
+    }
 }
 
 /// Created a store with no compiler and the given memory limit (in bytes)
