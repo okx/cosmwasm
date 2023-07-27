8ad5b0b6dc7ff93c22caa7ca190402e77607f5c7
jianguo <zjg555543@163.com>
2023-07-27 19:15:45 +0800

    update

--- packages/vm/src/calls.rs
+++ packages/vm/src/calls.rs
@@ -666,7 +666,8 @@ mod tests {
                 ))
             }
             err => panic!("Unexpected error: {err:?}"),
-        }
+        };
+        println!("fff {:?}", instance.get_gas_left());
     }
 
     #[test]
